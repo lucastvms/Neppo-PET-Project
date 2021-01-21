ad49b6ca3215d969146c317d4d9f587fa5842b6e
Lucas Vieira <ltv@ic.ufal.br>
2021-01-21 13:49:21 -0300

    almost finishing header and added some links into README.md

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import SideMenu from "../components/SideMenu";
+import {CssBaseline, makeStyles} from "@material-ui/core";
+import Header from "../components/Header";
+
+const useStyles = makeStyles({
+    appMain: {
+        paddingLeft: '320px',
+        width: '100%'
+    }
+})
 
 function App() {
+    const classes = useStyles();
+
     return (
         <>
             <SideMenu />
-            <div>
-                here we go
+            <div className={classes.appMain}>
+                <Header />
             </div>
+            <CssBaseline />
         </>
     );
 }
